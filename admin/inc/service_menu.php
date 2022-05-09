11b8930c6b981d9373aa220bbfc13d5c528d47d0
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-05-09 10:04:00 +0530

    service updates

---
+++ admin/inc/service_menu.php
@@ -0,0 +1,30 @@
+<nav id="sidebar" class="sidebar">
+            <a class="sidebar-brand" href="#">
+                <svg>
+                    <use xlink:href="#ion-ios-pulse-strong"></use>
+                </svg>
+                Home managment 
+            </a>
+            <div class="sidebar-content">
+                <div class="sidebar-user">
+                    <img src="../theme/img/avatars/param3.png" alt="no image">
+                    <div class="fw-bold">Param Patel</div>
+                    <small>Service panel</small>
+                </div>
+                <ul class="sidebar-nav">
+                    <li class="sidebar-header">
+                        Main
+                    </li>
+                    <li class="sidebar-item">
+                        <a  href="forgot_password.php"  class="sidebar-link ">
+                            <i class="align-middle "></i> <span class="align-middle">Dashboards</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a  href="category_management.php" class="sidebar-link collapsed">
+                            <i class="align-middle "></i> <span class="align-middle"> </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>    
+        </nav>
